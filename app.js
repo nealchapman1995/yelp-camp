c9c006e4741c6c9e38d59a7c7b3339c8ab4bfbb6
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2020-12-05 15:31:04 -0700

    seeded campgrounds

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.get('/', (req, res) => {
 	res.render('home');
 })
 
-app.get('/makecampground', async (req, res) => {
-	const camp = new Campground({title: 'my backyard', description: 'cheap camping'});
-	await camp.save();
-	res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
+	res.render('campgrounds/index', {campgrounds})
 })
