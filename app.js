dd66468ac97ecb78a47a4e28d1947ed17b179699
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2020-12-07 17:52:27 -0700

    Added the abliity to delete

--- app.js
+++ app.js
@@ -59,5 +59,12 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.put('/campgrounds/:id', async (req, res) => {
 	const {id} = req.params;
-	Campground.findByIdAndUpdate()
+	const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+	res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect('/campgrounds');
 })
