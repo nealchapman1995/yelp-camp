adf7ec4112175f85e786bb3e0cddbb9489e50ed3
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2020-12-14 09:35:13 -0700

    added partial

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -17,6 +18,7 @@ mongoose.connection.once('open', () => {
 	console.log('Database connected')
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views')
 app.set('views', path.join(__dirname, 'views'));
